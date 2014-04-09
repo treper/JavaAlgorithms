bf859e7737973da557959ae0747e0f6c9f133034
Daniel Tang <ataosky@gmail.com>
2014-04-09 21:39:51 +0800

    first commit

---
+++ src/com/ataosky/leetcode/UniquePaths.java
@@ -0,0 +1,23 @@
+package com.ataosky.leetcode;
+
+//============================================================================
+//Unique Paths
+//A robot is located at the top-left corner of a m x n grid (marked 'Start'
+//in the diagram below).
+//
+//The robot can only move either down or right at any point in time. The robot
+//is trying to reach the bottom-right corner of the grid (marked 'Finish' in
+//the diagram below).
+//
+//How many possible unique paths are there?
+//
+//Complexity:
+//DP, O(m*n) time, O(1) space
+//DP, O(m*n) time, O(n) space
+//Math, O(n) time, O(1) space
+//============================================================================
+
+
+public class UniquePaths {
+
+}
---
+++ src/com/tudou/test/IntTest.java
@@ -0,0 +1,5 @@
+package com.tudou.test;
+
+public class IntTest {
+
+}
