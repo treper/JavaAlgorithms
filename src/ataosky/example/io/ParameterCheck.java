bce2f093377c8e547e7519269ff8b80a222baac9
Daniel Tang <ataosky@gmail.com>
2014-06-15 15:11:58 +0800

    some guava test

---
+++ src/ataosky/example/io/ParameterCheck.java
@@ -0,0 +1,12 @@
+package ataosky.example.io;
+
+/**
+ * Created by tangning on 14-6-15.
+ * reference :http://www.cnblogs.com/peida/p/Guava_Preconditions.html
+ */
+public class ParameterCheck{
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ src/ataosky/stackoverflow/TransactionManager.java
@@ -0,0 +1,11 @@
+package ataosky.stackoverflow;
+
+/**
+ * Created by tangning on 14-6-14.
+ */
+public class TransactionManager {
+    private static final ThreadLocal<String> new ThreadLocal<String>();
+    public static
+
+    }
+}
